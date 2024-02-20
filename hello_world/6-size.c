f6354ad473c7c82036b0c713b4b4ac841b42e5d0
NadegeL <nadege.luthier@gmail.fr>
2024-02-20 12:59:46 +0100

    printf size of type

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,15 +6,12 @@
  */
 int main(void)
 {
-	int integerType;
-	float floatType;
-	double doubleType;
-	char charType;
 
-	printf("Size of int: %zu bytes\n"sizeof(integerType));
-	printf("Size of float:%zu bytes\n"sizeof(floatType));
- 	printf("Size of double:%zu bytes\n"sizeof(doubleType));
-	printf("Size of char:%zu bytes\n"sizeof(charType));
+	printf("Size of int: %zu bytes\n", sizeof(integerType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+ 	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of char: %zu bytes\n", sizeof(charType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 
 	return (0);
 }
