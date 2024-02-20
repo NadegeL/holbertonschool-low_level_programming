b6e7fe00e13012648d760642afa5588d5b92f04b
NadegeL <nadege.luthier@gmail.fr>
2024-02-20 13:09:03 +0100

    print size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,6 +12,7 @@ int main(void)
  	printf("Size of double: %ld byte(s)\n", sizeof(double));
 	printf("Size of char: %ld byte(s)\n", sizeof(char));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 
 	return (0);
 }
