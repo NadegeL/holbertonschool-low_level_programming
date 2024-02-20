120e788e597647a51c5686c8f3a5a97f3979009a
NadegeL <nadege.luthier@gmail.fr>
2024-02-20 13:17:14 +0100

    printf size type

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,12 +7,12 @@
 int main(void)
 {
 
-	printf("Size of int: %ld byte(s)\n", sizeof(int));
-	printf("Size of float: %ld byte(s)\n", sizeof(float));
- 	printf("Size of double: %ld byte(s)\n", sizeof(double));
-	printf("Size of char: %ld byte(s)\n", sizeof(char));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+ 	printf("Size of double: %lu byte(s)\n", sizeof(double));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 
 	return (0);
 }
