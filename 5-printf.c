9b4a8174214a9fa46ad2c15c73099488a7d03974
NadegeL <nadege.luthier@gmail.com>
2024-02-20 11:47:42 +0100

    printf

--- 5-printf.c
+++ 5-printf.c
@@ -1,2 +1,11 @@
-printf
-( "%s",with proper grammar, but the outcome is a piece of art)
+#include <stdio.h>
+/**
+ * main - code to put a message
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
