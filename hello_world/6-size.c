47ccaf0082011ad46c3a55f3e127c6bbd3851fce
NadegeL <nadege.luthier@gmail.fr>
2024-02-20 12:51:44 +0100

    printf size of types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	int integerType
-	float floatType
-	double doubleType
-	char charType
-	
+	int integerType;
+	float floatType;
+	double doubleType;
+	char charType;
+
 	printf("Size of int: %ld bytes\n"sizeof(integerType));
 	printf("Size of float:%ld bytes\n"sizeof(floatType));
  	printf("Size of double:%ld bytes\n"sizeof(doubleType));
