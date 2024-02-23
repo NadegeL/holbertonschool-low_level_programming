98d51890a4f7cf916f3c91e9d41a6ba1867cd38c
NadegeL <nadege.luthier@gmail.fr>
2024-02-23 15:39:37 +0100

    prints *10 alphabets

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet*10 - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+ void print_alphabet_x10(void)
+{
+int compteur = 0;
+
+while (compteur <10)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+	{
+
+		_putchar(letter);
+
+	}
+		_putchar('\n');
+		compteur++;
+
+}
+}
