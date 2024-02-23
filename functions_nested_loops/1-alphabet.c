b12adfc5e61043cfe3fe6fd8a7e739aa4f6b8fa6
NadegeL <nadege.luthier@gmail.fr>
2024-02-23 14:38:39 +0100

    alphabet sur ligne

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+	{
+
+		putchar(letter);
+
+	}
+		putchar('\n');
+
+}
+
+
+
