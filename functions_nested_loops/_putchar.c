7b03763ccd2ec416d3e26db0d84be7c8039ac037
NadegeL <nadege.luthier@gmail.fr>
2024-02-23 13:24:21 +0100

    templates

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -9,13 +9,5 @@
  */
 int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	
+	return (write(1, &c, 1));
 }
