e31a5e3ec15a4a737333c98845616e08294987b4
NadegeL <nadege.luthier@gmail.fr>
2024-02-24 19:48:27 +0100

    valeur absolue

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _abs - Checks absolute value.
+ * @r: The number checked.
+ *
+ * print absolute value of int.
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (0);
+	}
+	else{
+		return (-r);
+	}
+}
