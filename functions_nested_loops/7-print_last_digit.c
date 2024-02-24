1e4b951db64b15ec8cf422cc75fb65c08b410dde
NadegeL <nadege.luthier@gmail.fr>
2024-02-24 21:42:49 +0100

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,20 @@
 #include "main.h"
 
 /**
- * _print_last_digit(int) - prints the last digit.
+ * _print_last_digit - prints the last digit.
  *
- * @r: input number as an integer.
+ * @n: input number last digit.
  *
- * Return: absolute value
+ * Returns the value of the last digit.
  */
+print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
