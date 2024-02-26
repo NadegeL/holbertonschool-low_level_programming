7037fb9b2c0883256bf028a9452843e82f8e24f8
NadegeL <nadege.luthier@gmail.fr>
2024-02-26 11:39:18 +0100

    prints numbers without 2&4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - numbers from 0 à 9.
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+
+{
+	int numbers;
+
+	for (numbers = '0' ; numbers <= '9' ; numbers++)
+	{
+		if (numbers != '2'&& numbers != '4')
+		_putchar(numbers);
+	}
+		_putchar('\n');
+
+}
