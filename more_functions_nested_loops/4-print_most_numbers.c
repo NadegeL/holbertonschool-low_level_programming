6d5b3a7bc4c19dfdfc43da31b87123c7d937a2f2
NadegeL <nadege.luthier@gmail.fr>
2024-02-26 11:40:33 +0100

    prints numbers without 2&4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -11,7 +11,7 @@ void print_most_numbers(void)
 
 	for (numbers = '0' ; numbers <= '9' ; numbers++)
 	{
-		if (numbers != '2'&& numbers != '4')
+		if (numbers != '2' && numbers != '4')
 		_putchar(numbers);
 	}
 		_putchar('\n');
