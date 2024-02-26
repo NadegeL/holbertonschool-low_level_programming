d73f5776901dec262b9a4b482279456d71bdb25f
NadegeL <nadege.luthier@gmail.fr>
2024-02-26 11:09:36 +0100

    prints numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_numbers - Prints numbers from 0 à 9.
+ *@numbers:numbers
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	char numbers;
+
+	for (numbers = '0' ; numbers <= '9' ; numbers++)
+	{
+
+		putchar(numbers);
+
+	}
+		putchar('\n');
+
+}
+
+
+
