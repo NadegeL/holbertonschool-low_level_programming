3597488dd48e43baadfcf46d5d3617931b63a084
NadegeL <nadege.luthier@gmail.fr>
2024-02-26 17:30:14 +0100

    diagonalprint

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - Prints a diag.
+ *
+ *@n: number
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int size;
+	int hauteur;
+
+	if (n < 0)
+
+	{
+		_putchar('\n');
+	}
+	for (size = 0; size < n; size++)
+		{
+		for (hauteur = 0; hauteur < size; hauteur++)
+		{
+			_putchar(' ');
+		}
+		}
+	_putchar('\\');
+	_putchar('\n');
+
+}
+
