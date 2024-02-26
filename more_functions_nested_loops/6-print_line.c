7bd6b7830a2e6a5aea3b4db3179eb53131d7f0f0
NadegeL <nadege.luthier@gmail.fr>
2024-02-26 15:42:56 +0100

    print line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,14 +2,17 @@
 /**
  * print_line - Prints a line.
  *
+ *@n: number
+ *
  * Return: Always 0.
  */
- void print_line(int n)
- {
+void print_line(int n)
+{
 	int i;
-	for(i = 0; i < n; i++)
+
+	for (i = 0; i < n; i++)
 	{
 		_putchar('_');
 	}
 	_putchar('\n');
- }
+}
