f1844b8e2cfa893cd8c33cead87c3fafaf247a2f
NadegeL <nadege.luthier@gmail.fr>
2024-02-27 14:25:01 +0100

    swap a et b

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_in - swap a et b
+ *
+ *@a: a
+ *@b: b
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
