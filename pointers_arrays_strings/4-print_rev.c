445f7821eed7088ae343ca5e6c00b9ce485ec714
NadegeL <nadege.luthier@gmail.fr>
2024-02-28 12:38:50 +0100

    print reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,20 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
- * _print_rev - function that prints in rev.
+ * print_rev - function that prints in rev.
  *
- *@s: pointeur
+ *@s: strg
  * Return: Always 0.
  */
 void print_rev(char *s)
 {
-	while (*s)
-	{
-		_putchar (*s--);
-	}
-_putchar ('\n');
-}
+	int i;
+	int len = _strlen(s);
 
+	for (i = len - 1 ; i >= 0; i--)
+	{
+		_putchar (s[i]);
+	}
+	_putchar ('\n');
+}
