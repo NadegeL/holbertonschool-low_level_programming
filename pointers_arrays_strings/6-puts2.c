5b27586f56c2decc434d41164c033a3a7fffd3b3
NadegeL <nadege.luthier@gmail.fr>
2024-02-29 15:15:20 +0100

    puts 2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,15 +2,13 @@
 #include "2-strlen.c"
 
 /**
- * puts2(char *str) - every other character of a string
+ * puts2 - every other character of a string
  *
  *@str: strg
- *@i: index
- *@len: longueur
+ *
  * Return: Always 0.
  */
 void puts2(char *str)
-
 {
 	int i = 0;
 	int len = 0;
@@ -18,8 +16,8 @@ void puts2(char *str)
 	while (str[i++])
 		len++;
 	{
-		for(i = 0; i < len; i += 2)
-		_putchar ( str[i] );
+		for (i = 0; i < len; i += 2)
+		_putchar (str[i]);
 	}
 	_putchar('\n');
 }
