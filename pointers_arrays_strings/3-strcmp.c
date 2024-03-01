4f9c2e796ec6cb91803b4ef76d76fd6a9dec5e1c
NadegeL <nadege.luthier@gmail.fr>
2024-03-01 23:56:27 +0100

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,22 +9,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && (*s1 == *s2))
+	while (*s1 && *s2 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-	if (*s1 == *s2)
-    {
-        return 0;
-    }
-    else if (*s1 > *s2)
-    {
-        return 1;
-    }
-    else
-    {
-        return -1;
-    }
+	return (*s1 - *s2);
 }
 
