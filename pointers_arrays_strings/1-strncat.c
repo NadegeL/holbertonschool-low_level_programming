b5ecd7f43d2d733f3face58489fe50e67e9df420
NadegeL <nadege.luthier@gmail.fr>
2024-03-01 16:08:44 +0100

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,12 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0, dest_len = 0;
 
 	while (dest[i++])
-	{
-			dest_len++;
-	}
+{
+		dest_len++;
+}
 	for (i = 0; src[i] && i < n; i++)
-	{
+{
 		dest[dest_len++] = src[i];
-	}
+}
 	return (dest);
 }
