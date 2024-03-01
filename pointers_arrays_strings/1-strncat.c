6083a84a7ddb979e9586eacb20da6fae0d6c6107
NadegeL <nadege.luthier@gmail.fr>
2024-03-01 16:03:54 +0100

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncat - check the code
+ *
+ *@src: world
+ *@dest: hello
+ *@n: index
+ * Return: Always 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, dest_len = 0;
+
+	while (dest[i++])
+	{
+			dest_len++;
+	}
+	for (i = 0; src[i] && i < n; i++)
+	{
+		dest[dest_len++] = src[i];
+	}
+	return (dest);
+}
