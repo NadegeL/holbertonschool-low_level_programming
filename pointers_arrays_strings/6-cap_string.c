76baf1b11525e1c3fe61f1ac8423986bb2b25d42
NadegeL <nadege.luthier@gmail.fr>
2024-03-03 15:29:57 +0100

    change

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,25 +7,22 @@
  * string - input string
  * returns - the converted string
  */
+
+
 char *cap_string(char *str)
 {
 int i = 0;
 
-if (str[0] >= 'a' && str[0] <= 'z')
-{
-str[0] = str[0] - 32;
-}
 while (str[i] != '\0')
-{
-if (str[i] == ' ' || str[i] == '-' || str[i] == '.' || i == 0)
-{
-if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-{
-str[i + 1] = str[i + 1] - 32;
-}
-}
+    {
+if (islower(str[i]))
+    {
+if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '-' || str[i - 1] == '.')
+    {
+    str[i] = toupper(str[i]);
+    }
+    }
 i++;
-}
-
+    }
 return (str);
 }
