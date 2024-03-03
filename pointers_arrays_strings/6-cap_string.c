e538cadfb16f3be803ae2d51780ac34373e01677
NadegeL <nadege.luthier@gmail.fr>
2024-03-03 15:13:57 +0100

    code corrigé

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,28 @@
 #include "main.h"
-#include "ctype.h"
+#include <ctype.h>
 
 /*
-* string converted to Uppercase
-* cap_string - convertes string in uppercase
-*string'look up'
-* return 0
-*/
+ * string converted to Uppercase
+ * cap_string - converts string to uppercase
+ * string - input string
+ * returns - the converted string
+ */
 char *cap_string(char *str)
 {
 int i = 0;
-    /* Capitaliser la première lettre de la chaîne*/
-str[0] = toupper(str[0]);
 
-    /* Parcourir la chaîne caractère par caractère*/
+if (str[0] >= 'a' && str[0] <= 'z')
+{
+str[0] = str[0] - 32;
+}
 while (str[i] != '\0')
 {
-/* Si le caractère actuel est un espace, un tiret ou un point, alors*/
-/* capitaliser le prochain caractère s'il s'agit d'une lettre*/
 if (str[i] == ' ' || str[i] == '-' || str[i] == '.')
-	{
-if (isalpha(str[i + 1]))
-	{
-str[i + 1] = islower(str[i + 1]);
-	}
+{
+if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+{
+str[i + 1] = str[i + 1] - 32;
+}
 }
 i++;
 }
