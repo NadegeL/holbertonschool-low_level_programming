6dd8a199022154603d781c8f125ac8342a0055a0
NadegeL <nadege.luthier@gmail.fr>
2024-03-04 14:00:28 +0100

    memcpy function buffer

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - copie des emplacements de la mémoire
+ * @dest: the address of memory to print
+ * @src: the size of the memory to print
+ * @n: nb d'octets
+ *
+ * Return: Nothing.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+	dest[i] = src[i];
+	i++;
+	}
+return ('\0');
+}
+
