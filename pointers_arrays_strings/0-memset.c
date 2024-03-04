8f3578b064d09118458325374515c34bf2a22370
NadegeL <nadege.luthier@gmail.fr>
2024-03-04 15:49:23 +0100

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,21 +2,24 @@
 #include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
+ * _memset - function that fills memory with a constant byte.
+ * @b: the char
+ * @n: the
+ * @s:ptr
  * Return: Nothing.
  */
 char *_memset(char *s, char b, unsigned int n)
-{unsigned int i = 0;
 
-while( i < 0)
+{
+unsigned int i;
+
+i = 0;
+
+while (i < n)
 {
 s[i] = b;
 i++;
 }
-
 return (s);
 }
 
