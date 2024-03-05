e0063b4366294b7118d896e7631300d87fa9333f
NadegeL <nadege.luthier@gmail.fr>
2024-03-05 22:07:56 +0100

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ *strpbrk() - function locates the first occurrence in the string s
+ *
+ *@s: la chaine dans laquelle on chercher
+ *@accept: le nb de bytes recherchés
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+
+while (*s)
+{
+	for (i = 0; accept[i]; i++)
+	{
+		if (*s == accept[i])
+		return (s);
+	}
+	s++;
+}
+return (NULL);
+}
