25e36c4ad6ecf275005da1ad348f0396221000bb
NadegeL <nadege.luthier@gmail.fr>
2024-03-06 23:19:22 +0100

    strlen

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion -  prints a string in reverse.
+ *@s: ptr to the string
+ *Return: 0
+*/
+int _strlen_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
