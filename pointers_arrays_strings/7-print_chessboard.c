732db6c7bee28553afdd2d898f03eaf6911bb669
NadegeL <nadege.luthier@gmail.fr>
2024-03-06 10:55:09 +0100

    chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_chessboard - write a function that prints the chessboard.
+*
+*@a: chessboard to print
+*
+*Return (0);
+*/
+
+void print_chessboard(char (*a)[8])
+
+{
+	int i, j;
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+
+		_putchar('\n');
+	}
+}
