ea183af066090a61633b5e9dcc936b5ed45f059d
NadegeL <nadege.luthier@gmail.fr>
2024-03-07 14:41:06 +0100

    factorial n

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,6 +1,19 @@
 #include "main.h"
 /**
- * _print_rev_recursion -  prints a string in reverse.
- *@s: ptr to the string
+ * factorial -  return the factorial of a given number.
+ *@n: the factorial number
  *Return: 0
+ *
 */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+	return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
