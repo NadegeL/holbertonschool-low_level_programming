1ca0f04339f3bce1b0e1fb9d49025432a1836b15
NadegeL <nadege.luthier@gmail.fr>
2024-03-07 22:24:12 +0100

    nb premiers

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_sqrt_recursion -  find a square root of n.
+ *@n: entier dont on cherche la racine carrée.
+ *
+ *Return: 0
+ *
+ */
+int _sqrt_recursion(int n)
+{
+
+    int sqr = (int)sqrt(n);
+    int result = (int)(sqr);
+    return result;
+
+}
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *is_prime_number -   that returns 1 if the input integer is a prime number.
+ *@n: nombre premier
+ *
+ *Return: 1 if the input integer is a prime number, otherwise return 0
+ *
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)/*Check if n=1 or n=0*/
+	{
+		return (0);
+	}
+	if (n == 2)/*Check if n=2 */
+	{
+		return (1);
+	}
+	if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0))/*Check whether n is divisible by 2 or 3 or 5*/
+	{
+		return (0);
+	}
+	else if (n % 9 == 0 || n % 7 == 0)/*Check n is divisible by 9 or 7*/
+	{
+		return (0);
+	}
+	else
+		return (1);
+}
