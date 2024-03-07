78244a0997af8c6542c219ce9d07252a9bc05ebd
NadegeL <nadege.luthier@gmail.fr>
2024-03-07 21:13:06 +0100

    pow_recursion

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_pow_recursion -  returns the value of x raised to the power of y.
+ *@x: raised the power of y
+ *@y: puissance au carré
+ *Return: 0
+ *
+ */
+int _pow_recursion(int x, int y)
+{
+	if(y < 0)
+	{
+		return(-1);
+	}
+	if(y == 0)
+	{
+		return(1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
