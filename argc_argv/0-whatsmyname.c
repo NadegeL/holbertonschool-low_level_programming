6f215d44eb9169b230ec9945d9e2cacb0487ed33
NadegeL <nadege.luthier@gmail.fr>
2024-03-08 14:28:43 +0100

    whatsmyname

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-/*
+/**
+* main - fonction pour compter les arg
+* @argc: compteur
+* @argv: chaine de caractères
+*
+* Return: 0.
 */
 
 int main(int argc, char *argv[])
 {
-	if(argc==1)
-		printf("%s\n",argv[0]);
+	if (argc == 1)
+		printf("%s\n", argv[0]);
 	return (0);
 }
 
