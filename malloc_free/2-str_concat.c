a774f17e8c2ba8305c59d041edb321399b8ad4a9
NadegeL <nadege.luthier@gmail.fr>
2024-03-13 16:11:42 +0100

    Best School concatened

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,11 +16,11 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	int lenTotal;
 
-if (s1 == NULL || s2 == NULL)
-{
-s1 = "";
-s2 = "";
-}
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
+
 for (i = 0; s1[i]; i++)
 len1 = len1 + 1;
 
