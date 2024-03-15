68b104023b7791d47bb18afe24ee4a4f4d99eb12
NadegeL <nadege.luthier@gmail.fr>
2024-03-15 13:39:46 +0100

    config

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,17 +1,20 @@
-#ifndef dog
-#define dog
+#ifndef dog_h
+#define dog_h
 
 /**
- * main - check the code
- *
+ * struct dog - new description of structure
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
  * Return: Always 0.
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
-typedef struct dog my_dog;
+
+#endif
 
 
