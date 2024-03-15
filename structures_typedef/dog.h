3cb501bf7b92ace6422803cb19706b371d8b16e2
NadegeL <nadege.luthier@gmail.fr>
2024-03-15 15:05:05 +0100

    typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,9 +14,9 @@ struct dog
 	float age;
 	char *owner;
 };
-typedef struct dog my_dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 
 
 #endif
