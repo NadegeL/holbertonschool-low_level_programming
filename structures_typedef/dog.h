4c6541f5b81de0299568997a91d1e12a23307b3f
NadegeL <nadege.luthier@gmail.fr>
2024-03-15 12:19:31 +0100

    my_dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+int main()
+struct dog my_dog;
+
---
+++ structures_typedef/main.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+#include <time.h>
+
