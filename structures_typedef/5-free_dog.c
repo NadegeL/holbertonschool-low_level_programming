a06dacdcb7cb9edda2190a5d91f7010d87184cb7
NadegeL <nadege.luthier@gmail.fr>
2024-03-15 21:31:19 +0100

    freedogs

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,10 +1,13 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * 
- * 
- * 
-*/
+ * *new_dog- function that create a new dog
+ *strdup: returns a pointer to a new string
+ * @name:ptr
+ * @age: ..
+ * @owner: ..
+ * Return: Always 0.
+ */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
