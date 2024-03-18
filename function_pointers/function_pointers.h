b051728f3d76b8f3772faf6913e98af2ae8d2e32
NadegeL <nadege.luthier@gmail.fr>
2024-03-18 12:29:39 +0100

    define

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
-#define function_pointers.h
-#ifndef function_pointers.h
+#define function_pointers_h
+#ifndef function_pointer_h
 #include <stdlib.h>
+#include <stdio.h>
 
 
 void print_name(char *name, void (*f)(char *));
