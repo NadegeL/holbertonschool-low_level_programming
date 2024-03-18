58892a0b999adb99b05067f7b2be9727c14ff9c5
NadegeL <nadege.luthier@gmail.fr>
2024-03-18 15:12:41 +0100

    array iterator

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,11 +2,20 @@
 #include "function_pointers.h"
 /**
  * array_iterator - execute a function given on array
- * @action: ptr
- * @array:..
+ * @array: tableau d'entiers (int *array).
+ * @size:sa taille (size_t size).
+ * @action: fonction pointeur qui prend un entier
+ *  en argument et ne retourne rien (void (*action)(int)).
+ *
  * Return: Nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-
+if (array == NULL || action == NULL)
+	return;
+while (size-- > 0)
+	{
+	action(*array);
+	array++;
+	}
 }
