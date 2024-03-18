fccf64860729df3463b50f544fb4b6a07705545a
NadegeL <nadege.luthier@gmail.fr>
2024-03-18 14:44:05 +0100

    define

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,5 @@
-#ifndef function_pointer.h
-#define function_pointers.h
-
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef function_pointer_h
+#define function_pointers_h
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
