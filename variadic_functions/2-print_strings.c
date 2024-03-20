c16491d40a4f1b9a8518ce35067408e24abbba17
NadegeL <nadege.luthier@gmail.fr>
2024-03-20 16:16:12 +0100

    vf

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,13 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ *print_strings - print str
+ *@n: unsigned int
+ *@separator: strg of char
+ *Return: new line
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+}
---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ *print_strings - print str
+ *@n: unsigned int
+ *@separator: strg of char
+ *Return: new line
+*/
+void print_all(const char * const format, ...)
+{
+	
+}
