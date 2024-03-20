3bec7ef3962d52c1a50e4962ad3269a001864fec
NadegeL <nadege.luthier@gmail.fr>
2024-03-20 12:31:39 +0100

    betty

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -2,64 +2,42 @@
 #include <stdio.h>
 #include "3-calc.h"
 /**
- *op_add - addition
- *@a:nb to add
- *@b: nb to add
- *return: total op
+ *op_add - Returns the sum of two nb
+ *op_sub - Returns the difference of two nb
+ *op_mul - Returns the sum of two nb
+ *op_div - Returns the division of two nb
+ *op_mod - Returns the remainder of two nb
+ *@a:first nb
+ *@b:second nb
+ *return: result
 */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
-/**
- *op_sub - soustraction
- *@a:nb tu sub
- *@b: nb to sub
- *return: total sub
-*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
-/**
- * op_mul - multiplication
- * @a: nb to mul
- * @b: nb to mul
- * return: total multiplication
-*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-/**
- * op_div - division
- * @a: nb to divide
- * @b: nb to divide
- * return: total division
-*/
 int op_div(int a, int b)
 {
 	if (b  == 0)
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	return (a / b);
 }
-/**
- * op_mod - modulo
- * @a: nb to modulo
- * @b: nb to modulo
- * return: total modulo
-*/
 int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	return (a % b);
 }
