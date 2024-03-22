53d7ad6a37d8547420492338131071312e14479b
NadegeL <nadege.luthier@gmail.fr>
2024-03-22 11:25:12 +0100

    print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,11 +3,99 @@
 #include "variadic_functions.h"
 /**
  *print_strings - print str
- *@n: unsigned int
- *@separator: strg of char
+ *@char: prt sur str
+ *@separator: sep
  *Return: new line
+*/
+void print_string(va_list all)
+{
+	char *string;
+
+	string = va_arg(all, char *);
+
+	if (string == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", string);
+}
+/**
+ * print_char: char
+ *
+ * Return: new line
+*/
+void print_char(va_list all)
+{
+	printf("%c", va_arg(all, int));
+}
+/**
+ * print_int : int
+ *
+ * Return: new line
+*/
+void print_int(va_list all)
+{
+	printf("%d", va_arg(all, int));
+}
+/**
+ * print_float - float
+ *
+ * Return: new line
+*/
+void print_float(va_list all)
+{
+	printf("%f", va_arg(all, double));
+}
+/**
+ * print_all: all format
+ *
+ * Return: new line
 */
 void print_all(const char * const format, ...)
 {
-	
+	va_list all;
+	const char *separator = "";
+/**
+ * print_form - print all format
+ *
+ * @array: array of struct
+ * Return: new line
+*/
+struct print_form
+{
+	const char *c;
+	void (*f)(va_list);
+};
+
+struct print_form array[] = {
+	{"s", print_string},
+	{"c", print_char},
+	{"i", print_int},
+	{"f", print_float},
+	};
+	int i, j;
+
+	va_start(all, format);
+	i = 0;
+	separator = "";
+		while (format != NULL && format[i] != '\0')
+		{
+			j = 0;
+			while (j < 4)
+			{
+				if(format[i] == *array[j].c)
+				{
+					printf ("%s", separator);
+					array[j].f(all);
+					separator = ",";
+					break;
+				}
+			j++;
+			}
+		i++;
+		}
+	printf("\n");
+	va_end(all);
 }
+
